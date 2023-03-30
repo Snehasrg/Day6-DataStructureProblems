5d6ca59c359824cf3cafdb61d11e2d8bd4733ccb
Snehasrg <sghongade642@gmail.com>
2023-03-31 02:27:41 +0530

    [sneha]Added program for built in stack CRUD operation

---
+++ BuiltinStackCRUD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatastructureProblems
+{
+    public class BuiltinStackCRUD
+    {
+        public static void StackOperation()
+        {
+            Stack<string> intStack = new Stack<string>();
+            // Create operation
+            intStack.Push("Shital");
+            intStack.Push("Shreya");
+            intStack.Push("Pallavi");
+
+            //Read Operation
+            Console.WriteLine("\nElement Present in Stack are");
+            foreach (var item in intStack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nPeak Element is " + intStack.Peek());
+            //Delete Operation
+            Console.WriteLine("\nPopped Element is " + intStack.Pop());
+            Console.WriteLine("\nAfter Pop Operation elements in Stack");
+            foreach (var item in intStack)
+            {
+                Console.WriteLine(item);
+            }
+
+            //intStack.Clear();
+
+        }
+    }
+}
